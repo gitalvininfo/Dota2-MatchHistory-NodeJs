bbf3421d96009558d5af6b303ca4e61b701888a4
Alvin Yanson <yansonalvin29@gmail.com>
2021-10-28 22:57:26 +0800

    make it better

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const steam = require('./api_key.json');
-const bodyparser = require("body-parser");
-const cors = require("cors");
 const path = require('path');
 
 var distDir = __dirname + "/dist/";
@@ -13,21 +11,16 @@ app.use(express.static(distDir));
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(bodyparser.json());
-app.use(
-    bodyparser.urlencoded({
-        extended: true,
-    })
-);
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js,Express, and Postgres API' });
-});
+
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+
+app.get('/', (req, res) => {
+    res.json({ info: 'Node.js,Express, and Postgres API' });
 });
 
 
